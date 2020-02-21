292aa78267d02ff22de429c958ad45146f919182
dws-in <61307469+dws-in@users.noreply.github.com>
2020-02-21 14:43:46 +0700

    Update soal1.sh

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+#jawaban_1a
 awk 'BEGIN{FS = ","} {arr[$13]+=$NF} END {for (i in arr) {if(arr[i] != 0) print i,arr[i]}}' Sample-Superstore.csv | sort -gk2 | head -n1
+
+#jawaban_1b
+awk 'BEGIN{FS = ","} {if($13 == "Central") arr[$11]+=$NF} END {for (i in arr) {if(arr[i] != 0) print i,arr[i]}}' Sample-Superstore.csv | sort -nk2 | head -n2
+
+#jawaban_1c
+awk 'BEGIN{FS = ","} {if($11 == "Texas" || $11 == "Illinois") arr[$17] += $NF} END{for(i in arr) print i,arr[i]}' Sample-Superstore.csv | sort -nk2
